2ad7721fe3a88f0df6d78303d2730a2136131bce
GuntherReyscher <reyschergunther@gmail.com>
2024-08-08 11:29:17 -0300

    otra modificacion mensaje desarrollo

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div>
         <h1>Hola {API_URL}</h1>
-        <p>Este es el sitio en desarrollo</p>
+        <p>Este es el sitio en desarrollo ahora</p>
       </div>
     </>
   )
